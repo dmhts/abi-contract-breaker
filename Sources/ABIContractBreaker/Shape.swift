e3990fd07ab4fdd8d94c76a219542a3a11ef0a64
Dima Hutsuliak <dima.hutsuliak@mobimeo.com>
2024-03-23 17:23:52 +0100

    1.0.1

--- Sources/ABIContractBreaker/Shape.swift
+++ Sources/ABIContractBreaker/Shape.swift
@@ -4,6 +4,6 @@ import Foundation
     // These cases of an @frozen enum cannot be reordered.
     // The order of the cases with repect to each other
     // is part of the framework's binary interface.
-    case rect(w: Int, h: Int)
     case circle(radius: Int)
+    case rect(w: Int, h: Int)
   }
