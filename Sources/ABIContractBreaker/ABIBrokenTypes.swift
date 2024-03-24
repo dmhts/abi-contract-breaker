828bb78f42c67b377319a874b7b45588ab545100
Dima Hutsuliak <dima.hutsuliak@mobimeo.com>
2024-03-24 17:18:33 +0100

    Update 1.0.0

---
+++ Sources/ABIContractBreaker/ABIBrokenTypes.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+@frozen public enum FrozenShape {
+    case rect(w: Int, h: Int)
+    case circle(inRadian: Bool, radiusDescription: String)
+}
+
+public enum NonFrozenShape {
+    case rect(w: Int, h: Int)
+    case circle(inRadian: Bool, radiusDescription: String)
+}
+
+@frozen public struct FrozenAuto {
+    public let name: String
+    public let isElectric: Bool
+    public let maxSpeed: Float
+
+    init(name: String, isElectric: Bool, maxSpeed: Float) {
+        self.name = name
+        self.isElectric = isElectric
+        self.maxSpeed = maxSpeed
+    }
+}
+
+public struct NonFrozenAuto {
+    public let name: String
+    public let isElectric: Bool
+    public let maxSpeed: Float
+
+    init(name: String, isElectric: Bool, maxSpeed: Float) {
+        self.name = name
+        self.isElectric = isElectric
+        self.maxSpeed = maxSpeed
+    }
+}
+
+public class NonFinalMyAuto {
+    public let name: String
+    public let isElectric: Bool
+    public let maxSpeed: Float
+
+    init(name: String, isElectric: Bool, maxSpeed: Float) {
+        self.name = name
+        self.isElectric = isElectric
+        self.maxSpeed = maxSpeed
+    }
+}
--- Sources/ABIContractBreaker/Shape.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-@frozen public enum Shape {
-    // These cases of an @frozen enum cannot be reordered.
-    // The order of the cases with repect to each other
-    // is part of the framework's binary interface.
-    case circle(radius: Int)
-    case rect(w: Int, h: Int)
-  }
